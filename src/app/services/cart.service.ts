b67333f4e707db126f1548c958fe5467fc38fd6e
Dmitriy Glubokiy <glubokij85@gmail.com>
2020-01-10 18:54:18 +0200

    fixes

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { LocalService } from './local.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable()
 export class CartService {
-  public products$: BehaviorSubject<[]> = new BehaviorSubject([]);
+  private _products$: BehaviorSubject<[]>;
 
-  public get products(): [] {
-    return this.products$.getValue();
+  private get products(): [] {
+    return this._products$.getValue();
   }
 
   private readonly LOCAL_STORAGE_KEY = 'productsInCart';
@@ -15,23 +15,23 @@ export class CartService {
   constructor(private localService: LocalService) {
     const productsInCart = this.localService.getStorage(this.LOCAL_STORAGE_KEY) || [];
 
-    this.products$.next(productsInCart);
+    this._products$ = new BehaviorSubject(productsInCart);
 
-    this.products$.subscribe(products => {
+    this._products$.subscribe(products => {
       this.localService.setStorage(this.LOCAL_STORAGE_KEY, products);
     });
   }
 
-  // public watchLocalStorage(): BehaviorSubject<[]> {
-  //   return this.products.asObservable();
-  // }
+  public products$(): Observable<[]> {
+    return this._products$.asObservable();
+  }
 
   public addToCart(product: object): void {
-    this.products$.next([...this.products, product] as []);
+    this._products$.next([...this.products, product] as []);
   }
 
   public clear() {
-    this.products$.next([]);
+    this._products$.next([]);
   }
 
 
