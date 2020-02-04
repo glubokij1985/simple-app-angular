9b712f73680b9f3807ed0a5a11c55ce4985c512e
Dmitriy Glubokiy <glubokij85@gmail.com>
2020-02-04 13:26:36 +0200

    added providers for test

--- src/app/auth/containers/login/auth.component.spec.ts
+++ src/app/auth/containers/login/auth.component.spec.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './../../../shared/shared.module';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { AuthComponent } from './auth.component';
+import { AuthService } from '../../../core/auth/auth.service';
+
+class AuthServiceStub {
+  public login(): void { }
+}
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
@@ -11,7 +18,15 @@ describe('AuthComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthComponent ]
+      imports: [
+        CommonModule,
+        SharedModule
+      ],
+      declarations: [ AuthComponent ],
+      providers: [{
+        provide: AuthService,
+        useClass: AuthServiceStub,
+      }],
     })
     .compileComponents();
   }));
