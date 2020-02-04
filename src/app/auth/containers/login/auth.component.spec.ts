6033c6fdd56e1a953a8bafca2ec1bc4897060bd2
front-boombox <glubokij85@gmail.com>
2020-02-04 23:58:51 +0200

    dfs

--- src/app/auth/containers/login/auth.component.spec.ts
+++ src/app/auth/containers/login/auth.component.spec.ts
@@ -1,43 +1,67 @@
-import { CommonModule } from '@angular/common';
-import { SharedModule } from './../../../shared/shared.module';
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
 import { AuthService } from '../../../core/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './../../../shared/shared.module';
+import { LocalService } from '../../../core/storage/local.service';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EMPTY } from 'rxjs';
 
 class AuthServiceStub {
   public login(): void { }
 }
 
 describe('AuthComponent', () => {
-  let component: AuthComponent;
+  const localService: LocalService = new LocalService();
+  const service: AuthService = new AuthService(localService);
+  const component: AuthComponent = new AuthComponent(service, null);
+  let router: Router;
   let fixture: ComponentFixture<AuthComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        CommonModule,
-        SharedModule
-      ],
-      declarations: [ AuthComponent ],
-      providers: [{
-        provide: AuthService,
-        useClass: AuthServiceStub,
-      }],
-    })
-    .compileComponents();
-  }));
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({
+  //     imports: [
+  //       CommonModule,
+  //       SharedModule,
+  //       RouterTestingModule.withRoutes([]),
+  //     ],
+  //     declarations: [ AuthComponent ],
+  //     providers: [{
+  //       provide: AuthService,
+  //       useClass: AuthServiceStub,
+  //     }],
+  //   })
+  //   .compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AuthComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  //   fixture = TestBed.createComponent(AuthComponent);
+  //   router = TestBed.get(Router);
+  //   // component = new AuthComponent(new AuthService(new LocalService()), router);
+  //   // fixture.detectChanges();
+  // });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  xit('should create form with 2 controls', () => {
+    expect(component.loginForm.contains('email')).toBeTruthy();
+    expect(component.loginForm.contains('password')).toBeTruthy();
+  });
+
+  it('should be valid', () => {
+    const spy = spyOn(service, 'login').and.callFake(() => {
+      return EMPTY;
+    });
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  xit('should be created', inject([AuthService], (service: AuthService) => {
+    expect(component).toBeTruthy();
+  }));
 });
--- src/app/core/auth/auth.service.spec.ts
+++ src/app/core/auth/auth.service.spec.ts
@@ -1,8 +1,17 @@
 import { AuthService } from './auth.service';
+import { AuthComponent } from '../../auth/containers/login/auth.component';
 
-describe('isLoggedIn', () => {
+xdescribe('isLoggedIn', () => {
+    let component: AuthComponent;
+    let service: AuthService;
+
+    beforeEach(() => {
+        service = new AuthService(null);
+        component = new AuthComponent(service, null);
+    });
+
     it('should return true if user is logged in', () => {
-        const result = AuthService.prototype.isLoggedIn();
-        expect(result).toBe(true);
+        const result = component.loginForm.valid;
+        expect(result).toBeTruthy();
     });
 });
