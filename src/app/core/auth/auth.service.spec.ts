31e18668086c43f7a9c6eac5c67b4af0a91cadaf
front-boombox <glubokij85@gmail.com>
2020-02-11 23:46:20 +0200

    fix

--- src/app/core/auth/auth.service.spec.ts
+++ src/app/core/auth/auth.service.spec.ts
@@ -1,11 +1,25 @@
 import { AuthService } from './auth.service';
 import { AuthComponent } from '../../auth/containers/login/auth.component';
+import { TestBed } from '@angular/core/testing';
+import { LocalService } from '../storage/local.service';
+
+class LocalServiceStub {
+    public isLoggedIn(): void { }
+}
 
 describe('isLoggedIn', () => {
     // let component: AuthComponent;
     let service: AuthService;
 
     beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: LocalService,
+                    useClass: LocalServiceStub,
+                }
+            ]
+        });
         service = new AuthService(null);
         // component = new AuthComponent(service, null);
     });
