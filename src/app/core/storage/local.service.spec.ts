85b51b25d3d308301ea9f82f5ee0d6249a7f075f
Dmitriy Glubokiy <glubokij85@gmail.com>
2020-02-14 17:06:13 +0200

    fix local test

--- src/app/core/storage/local.service.spec.ts
+++ src/app/core/storage/local.service.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { LocalService } from './local.service';
 
+const userStub = {
+    email: 'test@mail.com',
+    password: 'psw',
+};
+
 describe('LocalService', () => {
     let service: LocalService;
 
@@ -12,23 +17,8 @@ describe('LocalService', () => {
         service = TestBed.get(LocalService);
     });
 
-    beforeEach(() => {
-        const store = {};
-        const localStorageStub = {
-            setStorage: (key: string, value: string) => {
-                store[key] = value;
-            },
-            getStorage: (key: string): string => {
-                return key in store ? store[key] : null;
-            },
-            removeStorage: (key: string) => {
-                delete store[key];
-            }
-        };
-
-        spyOn(localStorage, 'setItem').and.callFake(localStorageStub.setStorage);
-        spyOn(localStorage, 'getItem').and.callFake(localStorageStub.getStorage);
-        spyOn(localStorage, 'removeItem').and.callFake(localStorageStub.removeStorage);
+    afterEach(() => {
+        localStorage.removeItem('item');
     });
 
     it('should create local service', () => {
@@ -36,15 +26,13 @@ describe('LocalService', () => {
     });
 
     it('should set local storage item', () => {
-        service.setStorage('item', 'some_value');
-        expect(localStorage.getItem('item')).toMatch('some_value');
+        service.setStorage('item', userStub);
+        expect(localStorage.getItem('item')).toEqual(JSON.stringify(userStub));
     });
 
     it('should return local storage item', () => {
-        localStorage.setItem('item', 'some_value');
-        // debugger;
-        // let ggg = console.log(true);
-        expect(service.getStorage('item')).toEqual('some_value');
+        localStorage.setItem('item', JSON.stringify(userStub));
+        expect(service.getStorage('item')).toEqual(userStub);
     });
 
     it('should remove item from local storage', () => {
